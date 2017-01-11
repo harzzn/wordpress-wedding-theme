031d6e5e528df39f5d5b19aaea0c2095cc53718e
Harri Heljala <harri.heljala@gmail.com>
2017-01-11 11:43:35 +0200

    Responsiveness for footer.

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 			<footer class="col-xs-10 col-xs-offset-1">
-				<span class="pull-right text-muted">Website by <a href="https://helja.la" target="_blank">Harri Heljala</a></span>
+				<span class="pull-right text-muted hidden-xs">Website by <a href="https://helja.la" target="_blank">Harri Heljala</a></span>
 				<nav>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
 				</nav>
+				<div class="text-muted visible-xs-block text-center mt20">Website by <a href="https://helja.la" target="_blank">Harri Heljala</a></div>
 			</footer>
 		</div>
 
